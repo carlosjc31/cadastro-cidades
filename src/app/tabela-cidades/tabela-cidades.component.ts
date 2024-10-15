d1829d193dc54c51156064a8ebdf926e0a10ef94
carlosjc31 <carlosjc31@hotmail.com>
2024-10-15 17:32:12 -0300

    criar navbar

--- src/app/tabela-cidades/tabela-cidades.component.ts
+++ src/app/tabela-cidades/tabela-cidades.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { cidade } from '../cidades';
+import { cidadesService } from '../cidade.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabela-cidades',
   templateUrl: './tabela-cidades.component.html',
   styleUrl: './tabela-cidades.component.css'
 })
-export class TabelaCidadesComponent {
+export class TabelaCidadesComponent implements OnInit {
+  cidades: cidade[] = [];
+
+  constructor(private service: cidadesService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.loadcidades();
+  }
+  loadcidades() {
+    this.service.getcidades().subscribe({
+      next: data => this.cidades = data
+    });
+  }
+  delete(cidade: cidade) {
+    this.service.deletecidades(cidade).subscribe({
+      next: () => this.loadcidades()
+    })
+  }
 
 }
